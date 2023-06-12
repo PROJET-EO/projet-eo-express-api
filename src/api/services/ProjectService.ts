871f5e2407216519943f1665710b9364ab97cac7
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2023-06-12 08:53:39 +0300

    fix: user mapper

--- src/api/services/ProjectService.ts
+++ src/api/services/ProjectService.ts
@@ -5,57 +5,72 @@ import { ICreateProjectDTO } from "../models/dtos/project/ICreateProjectDTO";
 import { descriptionSetter } from "./utils/description";
 
 const createNewProject = async (ProjectDomain: ICreateProjectDTO) => {
-  ProjectDomain.createdAt = new Date().toString()
+  ProjectDomain.createdAt = new Date().toString();
   const newProject = await Project.create(ProjectDomain);
   return newProject;
 };
 
 const getProjectById = async (id: mongoose.Types.ObjectId | string) => {
-  const ProjectFinded = await Project.findById(id)
-    descriptionSetter(ProjectFinded!)
+  const ProjectFinded = await Project.findById(id);
+  descriptionSetter(ProjectFinded!);
+  return ProjectFinded;
+};
+const getProjectByName = async (name: string) => {
+  const ProjectFinded = await Project.findOne({ name: name });
+  descriptionSetter(ProjectFinded!);
   return ProjectFinded;
 };
 
 const getProjectByQuery = async (query: UserDocument) => {
-
   const ProjectWithQuery = await Project.findOne({
-    query
+    query,
   });
-  
-  descriptionSetter(ProjectWithQuery!)
+
+  descriptionSetter(ProjectWithQuery!);
   return ProjectWithQuery;
 };
-const UpdateProject =async (ProjectDomain : ICreateProjectDTO) => {
-  ProjectDomain.createdAt = new Date().toString()
-  const ProjectToUpdate = await Project.updateOne({ProjectDomain})
+const UpdateProject = async (ProjectDomain: ICreateProjectDTO) => {
+  ProjectDomain.createdAt = new Date().toString();
+  const ProjectToUpdate = await Project.updateOne({ ProjectDomain });
   return ProjectToUpdate;
-}
+};
 
-const getAllProjectQueried =async (req : any = {}) => {
-    const filteredQuery = {
-        metadata: {
-          $elemMatch: {
-            $and: Object.entries(req).map(([key, value]) => ({ [key]: value }))
-          }
-        }
-      };
-  if(req){
-    const queryiedProject = await Project.find(filteredQuery);
-    return queryiedProject
-  } 
-    const ProjectAll = await Project.find();
-    ProjectAll.forEach((e)=>descriptionSetter(e!))
-  return ProjectAll;
-}
-const getAllProject =async () => {
-    const ProjectAll = await Project.find();
-    ProjectAll.forEach((e)=>descriptionSetter(e!))
-    return ProjectAll;
+const getAllProjectQueried = async (req: any) => {
+  const filteredQuery = {
+    metadata: {
+      $elemMatch: {
+        $and: Object.entries(req).map(([key, value]) => ({ [key]: value }))[0],
+      },
+    },
+  };
+  if (req) {
+    const queryiedProject = await Project.find(req);
+    queryiedProject.forEach((e) => descriptionSetter(e!));
+    return queryiedProject;
   }
-const removeProject =async (id : string) => {
-     await Project.findByIdAndDelete(id);
-  
-}
+};
+const getAllProject = async () => {
+  const ProjectAll = await Project.find()
+    .exec()
+    .then((users) => {
+      const response = {
+        count: users.length,
+        users: users.map((user) => {
+          return {
+            _id: user._id,
+            name: user.name,
+            url: user.url,
+          };
+        }),
+      };
+      return response;
+    });
+
+  return ProjectAll;
+};
+const removeProject = async (id: string) => {
+  await Project.findByIdAndDelete(id);
+};
 
 const ProjectService = {
   createNewProject,
@@ -64,7 +79,7 @@ const ProjectService = {
   getAllProjectQueried,
   UpdateProject,
   removeProject,
-  getAllProject
+  getAllProject,
 };
 
 export default ProjectService;
