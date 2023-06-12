3ef7a0e024f6018360416d6aaf96412a364637b4
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-06-12 10:39:33 +0300

    fix: fix token verification

--- src/api/middleware/TokenMiddleware.ts
+++ src/api/middleware/TokenMiddleware.ts
@@ -10,7 +10,7 @@ export const verifyToken = (
   next: NextFunction
 ) => {
   const {headers}= req
-  const token = headers.authorization ;
+  const token = headers.authorization?.split(' ')[1] ;
   if (!token) return res.status(401).json({ error: "Access Denied" });
 
   try {
