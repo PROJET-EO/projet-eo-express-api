baf12e5dc664d4bba5bd506fe74e70e243dffc0a
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-06-12 10:41:00 +0300

    chore: add token verification on routes

--- src/api/routes/ProjectRoutes.ts
+++ src/api/routes/ProjectRoutes.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import projectController from "../controllers/ProjectController";
+import {verifyToken} from "../middleware/TokenMiddleware";
 //import { verifyToken } from "../middleware/TokenMiddleware";
 // import verifyToken to make jwt signing
 const router = express.Router();
 
 
 // route: /api/projects for all projects
-router.route('/projects').get(projectController.getAllProject);
+router.route('/projects').get(verifyToken, projectController.getAllProject);
 
 // route: /api/project/create for creating new project
-router.route('/project/update/:id').put(projectController.UpdateProject)
-router.route('/project/:id').delete(projectController.removeProject)
-router.route('/project/:id').get(projectController.getProjectById)
-router.route('/project').post(projectController.createProject)
+router.route('/project/update/:id').put(verifyToken, projectController.UpdateProject)
+router.route('/project/:id').delete(verifyToken, projectController.removeProject)
+router.route('/project/:id').get(verifyToken, projectController.getProjectById)
+router.route('/project').post(verifyToken, projectController.createProject)
 
 
 export = router;
