1b21b570f6ecb29fdde21125db2174ef87375d80
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2023-06-12 11:08:00 +0300

    chore: update model schema

--- src/api/models/schemas/ProjectSchema.ts
+++ src/api/models/schemas/ProjectSchema.ts
@@ -1,13 +1,53 @@
-import { Schema, model, Model } from "mongoose";
+import mongoose, { Schema, model, Model } from "mongoose";
 import { ProjectDocument } from "../documents";
 import { ProjectModel } from "../types/ProjectModel";
 
 let projectSchema = new Schema<ProjectDocument, ProjectModel>({
+  id: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
-    min: 3,
-    max: 255,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  contributors: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comment: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  reactor: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "React",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
